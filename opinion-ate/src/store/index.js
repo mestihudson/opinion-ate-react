d09b0c4febd190005f7eda65e1795f53bf042235
Hudson Ferreira Leite <mestihudson@gmail.com>
2025-04-23 19:18:54 -0300

    scaffold: add service to load restaurants from api

--- opinion-ate/src/store/index.js
+++ opinion-ate/src/store/index.js
@@ -1,8 +1,9 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 
+import api from '../api'
 import rootReducer from './reducers'
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const store = createStore(rootReducer, applyMiddleware(thunk.withExtraArgument(api)))
 
 export default store
