30aac90f86b3dcc3f633c4f6e8db5d5fc134b8bc
Hudson Ferreira Leite <mestihudson@gmail.com>
2025-04-23 01:20:42 -0300

    test(unit): should display the restaurants

--- opinion-ate/src/components/RestaurantList.spec.js
+++ opinion-ate/src/components/RestaurantList.spec.js
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import RestaurantList from './RestaurantList'
 
 describe('RestaurantList', () => {
@@ -8,5 +8,18 @@ describe('RestaurantList', () => {
     render(<RestaurantList loadRestaurants={loadRestaurants} />)
 
     expect(loadRestaurants).toHaveBeenCalled()
+  })
+
+  it('displays the restaurants', () => {
+    const noop = () => { }
+    const restaurants = [
+      { id: 1, name: 'Sushi Place' },
+      { id: 2, name: 'Pizza Place' },
+    ]
+
+    render(<RestaurantList loadRestaurants={noop} restaurants={restaurants} />)
+
+    expect(screen.getByText('Sushi Place')).toBeInTheDocument()
+    expect(screen.getByText('Pizza Place')).toBeInTheDocument()
   })
 })
