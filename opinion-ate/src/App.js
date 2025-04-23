f690dd4bb5fd3983228383bffda3a618e67b91cb
Hudson Ferreira Leite <mestihudson@gmail.com>
2025-04-23 06:47:31 -0300

    scaffold: wrap App on react-redux Provider component

--- opinion-ate/src/App.js
+++ opinion-ate/src/App.js
@@ -1,9 +1,12 @@
+import { Provider } from 'react-redux'
+
 import RestaurantScreen from './components/RestaurantScreen'
+import store from './store'
 
 export default function App() {
   return (
-    <div>
+    <Provider store={store}>
       <RestaurantScreen />
-    </div>
+    </Provider>
   )
 }
