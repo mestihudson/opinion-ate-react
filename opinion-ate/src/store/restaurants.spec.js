24dd96ada545476908812dc8bd340e9e7c40162d
Hudson Ferreira Leite <mestihudson@gmail.com>
2025-04-26 00:24:26 -0300

    refactor(test/unit): move out setup of test cases

--- opinion-ate/src/store/restaurants.spec.js
+++ opinion-ate/src/store/restaurants.spec.js
@@ -80,19 +80,23 @@ describe('restaurants', () => {
     })
 
     describe('while loading', () => {
-      it('sets a loading flag', () => {
+      let store
+
+      beforeEach(() => {
         const api = {
           loadRestaurants: () => new Promise(() => { }),
         }
         const initialState = {}
-        const store = createStore(
+        store = createStore(
           restaurantsReducer,
           initialState,
           applyMiddleware(thunk.withExtraArgument(api)),
         )
 
         store.dispatch(loadRestaurants())
+      })
 
+      it('sets a loading flag', () => {
         expect(store.getState().loading).toEqual(true)
       })
     })
