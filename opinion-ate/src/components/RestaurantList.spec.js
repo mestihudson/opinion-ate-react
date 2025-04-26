7dcbec5abfac8bdf6819572985c4c66cdc64343b
Hudson Ferreira Leite <mestihudson@gmail.com>
2025-04-25 22:39:40 -0300

    test(unit): should display the loading indicator while loading

--- opinion-ate/src/components/RestaurantList.spec.js
+++ opinion-ate/src/components/RestaurantList.spec.js
@@ -32,4 +32,10 @@ describe('RestaurantList', () => {
     expect(screen.getByText('Sushi Place')).toBeInTheDocument()
     expect(screen.getByText('Pizza Place')).toBeInTheDocument()
   })
+
+  it('displays the loading indicator while loading', () => {
+    renderComponent({ loading: true })
+
+    expect(screen.getByRole('progressbar')).toBeInTheDocument()
+  })
 })
