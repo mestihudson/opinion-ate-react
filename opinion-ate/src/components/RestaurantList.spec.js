e62648633f1b0a87d1dd4d6fbbd887925ac1dfd8
Hudson Ferreira Leite <mestihudson@gmail.com>
2025-04-25 23:25:34 -0300

    refactor(test/unit): move related test cases to same context place

--- opinion-ate/src/components/RestaurantList.spec.js
+++ opinion-ate/src/components/RestaurantList.spec.js
@@ -20,28 +20,30 @@ describe('RestaurantList', () => {
     render(<RestaurantList{...props} />)
   }
 
+  describe('when loading succeeds', () => {
+    it('displays the restaurants', () => {
+      renderComponent()
+
+      expect(screen.getByText('Sushi Place')).toBeInTheDocument()
+      expect(screen.getByText('Pizza Place')).toBeInTheDocument()
+    })
+
+    it('does not display the loading indicator while not loading', () => {
+      renderComponent({ loading: false })
+
+      expect(screen.queryByRole('progressbar')).not.toBeInTheDocument()
+    })
+  })
+
   it('loads restaurants on first render', () => {
     renderComponent()
 
     expect(loadRestaurants).toHaveBeenCalled()
   })
 
-  it('displays the restaurants', () => {
-    renderComponent()
-
-    expect(screen.getByText('Sushi Place')).toBeInTheDocument()
-    expect(screen.getByText('Pizza Place')).toBeInTheDocument()
-  })
-
   it('displays the loading indicator while loading', () => {
     renderComponent({ loading: true })
 
     expect(screen.getByRole('progressbar')).toBeInTheDocument()
-  })
-
-  it('does not display the loading indicator while not loading', () => {
-    renderComponent({ loading: false })
-
-    expect(screen.queryByRole('progressbar')).not.toBeInTheDocument()
   })
 })
