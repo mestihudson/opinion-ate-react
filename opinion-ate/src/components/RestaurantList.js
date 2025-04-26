5c91166d2ea686b45749cb49f169adaa86b758ea
Hudson Ferreira Leite <mestihudson@gmail.com>
2025-04-26 00:13:27 -0300

    feat: does not display the error message

--- opinion-ate/src/components/RestaurantList.js
+++ opinion-ate/src/components/RestaurantList.js
@@ -9,7 +9,7 @@ import { connect } from 'react-redux'
 
 import { loadRestaurants } from '../store/restaurants/actions'
 
-export function RestaurantList({ loadRestaurants, restaurants, loading }) {
+export function RestaurantList({ loadRestaurants, restaurants, loading, loadError }) {
   useEffect(() => {
     loadRestaurants()
   }, [loadRestaurants])
@@ -17,7 +17,7 @@ export function RestaurantList({ loadRestaurants, restaurants, loading }) {
   return (
     <>
       {loading && <CircularProgress />}
-      <Alert severity="error">Restaurants could not be loaded.</Alert>
+      {loadError && (<Alert severity="error">Restaurants could not be loaded.</Alert>)}
       <List>
         {restaurants.map((restaurant) => (
           <ListItem key={restaurant.id}>
