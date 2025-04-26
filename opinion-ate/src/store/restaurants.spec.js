0fa45ed3798d6f3970d7a724f957199e0e0dcd69
Hudson Ferreira Leite <mestihudson@gmail.com>
2025-04-25 23:42:14 -0300

    refactor(test/unit): move out fixture

--- opinion-ate/src/store/restaurants.spec.js
+++ opinion-ate/src/store/restaurants.spec.js
@@ -7,11 +7,12 @@ import restaurantsReducer from './restaurants/reducers'
 describe('restaurants', () => {
   describe('loadRestaurants action', () => {
     describe('when loading succeeds', () => {
+      const records = [
+        { id: 1, name: 'Sushi Place' },
+        { id: 2, name: 'Pizza Place' },
+      ]
+
       it('stores the restaurants', async () => {
-        const records = [
-          { id: 1, name: 'Sushi Place' },
-          { id: 2, name: 'Pizza Place' },
-        ]
         const api = {
           loadRestaurants: () => Promise.resolve(records),
         }
