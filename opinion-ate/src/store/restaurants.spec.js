543d3182481d1934ab500d55f07af8845dcb21aa
Hudson Ferreira Leite <mestihudson@gmail.com>
2025-04-25 23:33:08 -0300

    refactor(test/unit): move test case to new context

--- opinion-ate/src/store/restaurants.spec.js
+++ opinion-ate/src/store/restaurants.spec.js
@@ -6,26 +6,28 @@ import restaurantsReducer from './restaurants/reducers'
 
 describe('restaurants', () => {
   describe('loadRestaurants action', () => {
-    it('stores the restaurants', async () => {
-      const records = [
-        { id: 1, name: 'Sushi Place' },
-        { id: 2, name: 'Pizza Place' },
-      ]
-      const api = {
-        loadRestaurants: () => Promise.resolve(records),
-      }
-      const initialState = {
-        records: [],
-      }
-      const store = createStore(
-        restaurantsReducer,
-        initialState,
-        applyMiddleware(thunk.withExtraArgument(api)),
-      )
+    describe('when loading succeeds', () => {
+      it('stores the restaurants', async () => {
+        const records = [
+          { id: 1, name: 'Sushi Place' },
+          { id: 2, name: 'Pizza Place' },
+        ]
+        const api = {
+          loadRestaurants: () => Promise.resolve(records),
+        }
+        const initialState = {
+          records: [],
+        }
+        const store = createStore(
+          restaurantsReducer,
+          initialState,
+          applyMiddleware(thunk.withExtraArgument(api)),
+        )
 
-      await store.dispatch(loadRestaurants())
+        await store.dispatch(loadRestaurants())
 
-      expect(store.getState().records).toEqual(records)
+        expect(store.getState().records).toEqual(records)
+      })
     })
   })
 })
