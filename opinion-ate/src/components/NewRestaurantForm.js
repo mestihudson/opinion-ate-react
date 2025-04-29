8e20a88fdfccac1a1020cdd8b43e481aa6d0ee6f
Hudson Ferreira Leite <mestihudson@gmail.com>
2025-04-28 23:18:41 -0300

    scaffold: add Add button to NewRestaurantForm component

--- opinion-ate/src/components/NewRestaurantForm.js
+++ opinion-ate/src/components/NewRestaurantForm.js
@@ -1,9 +1,13 @@
 import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
 
 export function NewRestaurantForm() {
   return (
     <form>
       <TextField placeholder="Add Restaurant" fullWidth variant="filled" />
+      <Button variant="contained" color="primary">
+        Add
+      </Button>
     </form>
   )
 }
