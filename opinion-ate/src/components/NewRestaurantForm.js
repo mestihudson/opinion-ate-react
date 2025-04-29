1761c4f9437d993ff5b38f717f7ddffd9ad89b31
Hudson Ferreira Leite <mestihudson@gmail.com>
2025-04-28 23:12:39 -0300

    scaffold: add NewRestaurantForm component

---
+++ opinion-ate/src/components/NewRestaurantForm.js
@@ -0,0 +1,11 @@
+import TextField from '@mui/material/TextField'
+
+export function NewRestaurantForm() {
+  return (
+    <form>
+      <TextField placeholder="Add Restaurant" fullWidth variant="filled" />
+    </form>
+  )
+}
+
+export default NewRestaurantForm
